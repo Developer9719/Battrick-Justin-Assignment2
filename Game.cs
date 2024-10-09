7d30424c8511f568acad4534fceabb37ffe90054
Justin Battrick <justin.bat19@hotmail.com>
2024-10-09 11:37:41 -0400

    Basic Setup Done - Window Title - Window Size - Set up array - Set up white background

--- Game.cs
+++ Game.cs
@@ -11,14 +11,21 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        string[] imageChoices = new string[5];
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Finish the Image");
+            Window.SetSize(800, 600);
 
+            imageChoices[0] = "Tree";
+            imageChoices[1] = "Car";
+            imageChoices[2] = "Plane";
+            imageChoices[3] = "Stickman";
+            imageChoices[4] = "House";
         }
 
         /// <summary>
@@ -26,7 +33,7 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
-
+            Window.ClearBackground(Color.White);
         }
     }
 }
